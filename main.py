2d9fd9d20fdc38486ab48a4f39fde6bdd2d2de5d
alexander-rojasr <alexaderojas09@gmail.com>
2026-06-11 22:35:46 -0500

    Se corrige estructura de bienvenida

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-print("Bienvenido a la piramide más grande del mundo, donde no se habia contado pero dentro de esta piramide existe un laberinto infinito con acertijos y trampas, pero si logramos avanzar llegaremos a la cima sin problema. Por el momento echemos un vistazo a los primeros cinco niveles.") 
+nombre = input("¿Cuál es tu nombre?\n") 
+print(f"Hola, {nombre}! Bienvenido a la piramide más grande del mundo, donde no se habia contado pero dentro de esta piramide existe un laberinto infinito con acertijos y trampas, pero si logramos avanzar llegaremos a la cima sin problema. Por el momento echemos un vistazo a los primeros cinco niveles.")
