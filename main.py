3f73cf2e3f4d2018fda5166c1bf316525f7d1df1
alexander-rojasr <alexaderojas09@gmail.com>
2026-06-11 22:30:34 -0500

    Bienvenida

---
+++ main.py
@@ -0,0 +1,1 @@
+print("Bienvenido a la piramide más grande del mundo, donde no se habia contado pero dentro de esta piramide existe un laberinto infinito con acertijos y trampas, pero si logramos avanzar llegaremos a la cima sin problema. Por el momento echemos un vistazo a los primeros cinco niveles.") 
